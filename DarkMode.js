fd8dae4af5640d68dc2103c1afb695f11a86e8a7
Mohammad Zaidi <77636535+Mzaidiwb@users.noreply.github.com>
2022-11-13 18:48:20 -0500

    Updated dark mode switch to change icons

--- DarkMode.js
+++ DarkMode.js
@@ -1,4 +1,15 @@
 function DarkMode() {
     var element = document.body;
     element.classList.toggle("dark-mode");
+    let moon = document.getElementById('moon')
+    let sun = document.getElementById('sun')
+
+    if (sun.getAttribute('display') == 'none') {
+        moon.setAttribute('display', 'none')
+        sun.setAttribute('display', 'block')
+    }
+    else if (sun.getAttribute('display') == 'block') {
+        moon.setAttribute('display', 'block')
+        sun.setAttribute('display', 'none')
+    }
 }
