4ac3b11e8dbf9f26151c36cce294277e9d6a2d92
Mohammad Zaidi <77636535+Mzaidiwb@users.noreply.github.com>
2022-12-03 18:54:19 -0500

    Removed projects fixed dark mode

--- DarkMode.js
+++ DarkMode.js
@@ -1,15 +1,30 @@
 function DarkMode() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
-    let moon = document.getElementById('moon')
-    let sun = document.getElementById('sun')
+    if (localStorage.getItem('dark', this.checked)) {
+        localStorage.removeItem('dark', this.checked);
+        document.body.classList.toggle("dark-mode");
+    }
+    else if (localStorage.getItem('dark', this.checked) === null) {
+        localStorage.setItem('dark', this.checked);
+        document.body.classList.toggle("dark-mode");
+    }
+
+    //Controls Icons for Dark Mode
+    let moon = document.getElementById('moon');
+    let sun = document.getElementById('sun');
 
     if (sun.getAttribute('display') == 'none') {
-        moon.setAttribute('display', 'none')
-        sun.setAttribute('display', 'block')
+        moon.setAttribute('display', 'none');
+        sun.setAttribute('display', 'block');
     }
     else if (sun.getAttribute('display') == 'block') {
-        moon.setAttribute('display', 'block')
-        sun.setAttribute('display', 'none')
+        moon.setAttribute('display', 'block');
+        sun.setAttribute('display', 'none');
     }
-}
+};
+
+window.onload = (event) => {
+    if (localStorage.getItem('dark', this.checked)) {
+        document.body.classList.toggle("dark-mode");
+    }
+
+};
